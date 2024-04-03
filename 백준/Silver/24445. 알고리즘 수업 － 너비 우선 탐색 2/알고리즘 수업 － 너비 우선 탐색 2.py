27f4e706a3225c23f2b2ddf49b7855bfbaa63f07
Jiyeon Son <162339630+fooooty@users.noreply.github.com>
2024-04-03 16:54:39 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 2368 ms, Memory: 340812 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10**5)
+#sys.stdin=open("input.txt","r")
+def bfs(maplist,r):
+  global check,visitlist,ans
+  visitlist.pop(0)
+  for i in maplist[r]:
+    if check[i]==0:
+      visitlist.append(i)
+      check[i]=ans
+      ans+=1
+  if visitlist:
+    bfs(maplist,visitlist[0])
+n,m,r=map(int,sys.stdin.readline().split())
+maplist=[[] for i in range(n+1)]
+visitlist=[]
+ans=1
+check=[0]*(n+1)
+for i in range(m):
+  a,b=map(int,sys.stdin.readline().split())
+  maplist[a].append(b)
+  maplist[b].append(a)
+for i in range(n+1):
+  maplist[i]=sorted(maplist[i],key=lambda x:(-x))
+visitlist.append(r)
+check[r]=ans
+ans+=1
+bfs(maplist,r)
+for i in range(1,n+1):
+  print(check[i])
